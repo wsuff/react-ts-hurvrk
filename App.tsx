81d64be83231b1f54b45ba6e9fc1c01b4d690ad1
wsuff <william.suffill@gmail.com>
2023-02-15 21:16:06 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { StateMachineProvider, createStore } from "little-state-machine";
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-hurvrk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-hurvrk)
