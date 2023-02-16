1bc2c5f800c9df3b4b941d33227da67c2e4f1269
wsuff <william.suffill@gmail.com>
2023-02-15 21:23:45 -0500

    2 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { StateMachineProvider, createStore } from "little-state-machine";
+import Step1 from "./Step1";
 
+import "./styles.css";
+
+createStore({
+  data: {}
+});
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <StateMachineProvider>
+      <h1>Page Form Wizzard</h1>
+
+      <Router>
+        <Route exact path="/" component={Step1} />
+        <Route path="/step2" component={Step2} />
+        <Route path="/result" component={Result} />
+      </Router>
+    </StateMachineProvider>
   );
 }
+
+exp
